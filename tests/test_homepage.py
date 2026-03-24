d735b768bd7c43fc60c11656031d4eed360dc5b4
ZYang <zhipeng.yang@internetbrands.com>
2026-03-24 17:37:43 +0800

    Commit 3: test github workflow

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -8,4 +8,8 @@ def test_meta():
 
 # create a test which have marker  "homepage"
 def test_error():
-    assert 1=10
+    assert 1 == 2
+
+
+def test_error2():
+    pytest.xfail('I want to set this fail')
