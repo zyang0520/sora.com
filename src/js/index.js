96e2450ed8f2b2164198a538cc87c21b4c801b3b
ZYang <zhipeng.yang@internetbrands.com>
2026-03-26 10:59:26 +0800

    CC-3: Add runConsole

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,15 @@ function getCookie() {
   }
   return cookieObj;
 }
+
+function runConsole() {
+  var cookieObj = getCookie();
+  if (cookieObj['sora_console'] === 'true') {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/eruda';
+    document.body.appendChild(script);
+    script.onload = function() {
+      eruda.init();
+    };
+  }
+}
