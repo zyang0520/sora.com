9a561d99c67d2fd98bd39bdd24869103750bcf70
ZYang <zhipeng.yang@internetbrands.com>
2026-03-26 09:54:26 +0800

    CC-1: Add index.js

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+function addMeta() {
+    var meta = document.createElement('meta');
+    meta.name = "viewport";
+    meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+}
