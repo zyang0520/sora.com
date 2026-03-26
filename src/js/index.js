d5d402befe4f1c2c500f2da4c8b58cbe111f8895
zyang0520 <167486144+zyang0520@users.noreply.github.com>
2026-03-26 10:03:01 +0800

    CC-1: Add index.js (#1)
    * CC-1: Add index.js

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+function addMeta() {
+    var meta = document.createElement('meta');
+    meta.name = "viewport";
+    meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+}
+
+function getCookie() {
+  var cookies = document.getCookie.split(';');
+  var cookieObj = {};
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    var parts = cookie.split('=');
+    var name = parts[0];
+    var value = parts[1];
+    cookieObj[name] = value;
+  }
+  return cookieObj;
+}
