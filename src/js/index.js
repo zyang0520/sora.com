dfb4691f9fb83a764760fb85373561e9a51fa487
ZYang <zhipeng.yang@internetbrands.com>
2026-03-26 10:01:46 +0800

    CC-2: Add getCookie

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,16 @@ function addMeta() {
     meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no";
     document.getElementsByTagName('head')[0].appendChild(meta);
 }
+
+function getCookie() {
+  var cookies = document.getCookie.split(';');
+  var cookieObj = {};
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    var parts = cookie.split('=');
+    var name = parts[0];
+    var value = parts[1];
+    cookieObj[name] = value;
+  }
+  return cookieObj;
+}
